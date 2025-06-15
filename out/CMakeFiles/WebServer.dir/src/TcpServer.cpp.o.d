b864b45bb146e0e1b154cac1410d9427d09e729e
atk-sheep <cyy_work@163.com>
2025-06-15 18:51:12 +0800

    fix untrack ignored dirs and files

--- out/CMakeFiles/WebServer.dir/src/TcpServer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/WebServer.dir/src/TcpServer.cpp.o: \
- /home/cyyu20/webserver/WebServer-sheep/src/TcpServer.cpp \
- /usr/include/stdc-predef.h
--- out/CMakeFiles/WebServer.dir/src/baseData.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/WebServer.dir/src/baseData.cpp.o: \
- /home/cyyu20/webserver/WebServer-sheep/src/baseData.cpp \
- /usr/include/stdc-predef.h \
- /home/cyyu20/webserver/WebServer-sheep/include/baseData.hpp
